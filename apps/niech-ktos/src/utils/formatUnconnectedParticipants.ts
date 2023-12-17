2a171f0a37a7bb5284d18f75045eb8b924f61723
Komoszek <komoszek10@gmail.com>
2023-12-17 12:48:57 +0100

    User formatUserMention in formatUnconnectedParticipants

--- apps/niech-ktos/src/utils/formatUnconnectedParticipants.ts
+++ apps/niech-ktos/src/utils/formatUnconnectedParticipants.ts
@@ -1,9 +1,11 @@
+import { formatUserMention } from "./formatUserMention";
+
 export function formatUnconnectedParticipants(unconnectedParticipantIds: string[]) {
     if (unconnectedParticipantIds.length === 0) {
         return "";
     }
 
     return `Następujące osoby nie znalazły się w rankingu (brak połączenia konta ze Splitwise'em :pepe_police:): ${unconnectedParticipantIds
-        .map(participantId => `<@${participantId}>`)
+        .map(formatUserMention)
         .join(", ")}`;
 }
