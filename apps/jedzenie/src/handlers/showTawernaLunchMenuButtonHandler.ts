5b49abdff94740e5dbbca6191aed40a8d491de36
Łukasz Komoszyński <lukasz.komoszynski@leancode.pl>
2024-01-31 10:23:28 +0100

    Fix jedzenie modal not opening

--- apps/jedzenie/src/handlers/showTawernaLunchMenuButtonHandler.ts
+++ apps/jedzenie/src/handlers/showTawernaLunchMenuButtonHandler.ts
@@ -1,5 +1,6 @@
 import { ActionArgs } from "./types";
 import { getTawernaLunchMenuMessageBlocks } from "./tawernaCommandHandler";
+import { View } from "@slack/bolt";
 
 export async function showTawernaLunchMenuButtonHandler({ ack, client, body }: ActionArgs) {
     await ack();
@@ -8,21 +9,30 @@ export async function showTawernaLunchMenuButtonHandler({ ack, client, body }: A
         return;
     }
 
-    await client.views.open({
+    const commonViewProps = {
+        type: "modal",
+        title: {
+            type: "plain_text",
+            text: "Tawerna - Lunch Menu",
+            emoji: true,
+        },
+        close: {
+            type: "plain_text",
+            text: "Fajno",
+            emoji: true,
+        },
+    } as const satisfies Partial<View>;
+
+    const { view } = await client.views.open({
         trigger_id: body.trigger_id,
         view: {
-            type: "modal",
-            blocks: await getTawernaLunchMenuMessageBlocks(),
-            title: {
-                type: "plain_text",
-                text: "Tawerna - Lunch Menu",
-                emoji: true,
-            },
-            close: {
-                type: "plain_text",
-                text: "Fajno",
-                emoji: true,
-            },
+            ...commonViewProps,
+            blocks: [],
         },
+    });
+
+    client.views.update({
+        view_id: view?.id,
+        view: { ...commonViewProps, blocks: await getTawernaLunchMenuMessageBlocks() },
     });
 }
