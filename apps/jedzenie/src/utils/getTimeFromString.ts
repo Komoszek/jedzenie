e4c7d980c9c7bc8830b1d8c902ba895c3c111276
Komoszek <komoszek10@gmail.com>
2024-04-06 00:22:51 +0200

    Fix getTimeFromString

--- apps/jedzenie/src/utils/getTimeFromString.ts
+++ apps/jedzenie/src/utils/getTimeFromString.ts
@@ -3,16 +3,35 @@ import { clamp } from "./clamp";
 // [hour, minutes]
 export type Time = [number, number];
 
+// TODO: use regex
 export function getTimeFromString(timeString: string): Time {
-    let [hour = 0, minutes = 0] = timeString
-        .split(":")
-        .slice(0, 2)
-        .map(v => parseInt(v));
+    if (timeString.length > 5) {
+        throw new Error("Invalid time string");
+    }
 
-    // change e.g. 911, 2136 hours into [9, 11] and [21, 36]
-    if (hour > 99) {
-        minutes = hour % 100;
-        hour = Math.floor(hour / 100);
+    let hour = 0,
+        minutes = 0;
+    const timeParsts = timeString.split(":");
+
+    if (timeParsts.length > 2 || timeParsts.length === 0) {
+        throw new Error("Invalid time string");
+    }
+
+    if (timeParsts.length === 1) {
+        const hourString = timeParsts[0];
+
+        if (hourString.length === 3) {
+            throw new Error("Invalid time string");
+        }
+
+        hour = parseInt(hourString.slice(0, 2));
+
+        if (hourString.length === 4) {
+            minutes = parseInt(hourString.slice(-2));
+        }
+    } else {
+        hour = parseInt(timeParsts[0]);
+        minutes = parseInt(timeParsts[1]);
     }
 
     return [clamp(0, hour, 23), clamp(0, minutes, 59)];
